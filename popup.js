da90c9c7d4fbe26c42a5f96882ff1a49a1e18d80
AaronLiu <rayandben2000@yahoo.com.tw>
2023-03-04 21:10:21 +0800

    feat - check current page & login status

--- popup.js
+++ popup.js
@@ -3,7 +3,7 @@ const reserveTime = document.getElementById("reserveTime");
 const reserveButton = document.getElementById("reserveButton");
 const message = document.getElementById("msg");
 
-const reserve = (date, startTimes) =>{
+const reserve = async (date, startTimes) => {
     const courts = [
         1112, // 5-1
         1113, // 5-2
@@ -16,19 +16,24 @@ const reserve = (date, startTimes) =>{
         1166, // 5-9
     ];
 
+    let resultMsg = 'Done';
     if (!startTimes || !date) {
         console.error('date & startTimes are required.');
         return;
     } else {
-        courts.map(court => {
-            startTimes.map(startTime => {
-                fetch(
+        await Promise.all(courts.map(async court => {
+            await Promise.all(startTimes.map(async startTime => {
+                const response = await fetch(
                     `https://bwd.xuanen.com.tw/wd02.aspx?module=net_booking&files=booking_place&StepFlag=25&QPid=${court}&QTime=${startTime}&PT=1&D=${date}`
                 );
-            });
-        });
-
+                if (response.url.indexOf("login") != -1) {
+                    resultMsg = "You must login first.";
+                }
+            }));
+        }));
     }
+
+    return resultMsg;
 };
 
 async function getCurrentTab() {
@@ -47,6 +52,11 @@ reserveDate.setAttribute("max", eightDay.toISOString().split("T")[0]);
 // Set button handler
 reserveButton.addEventListener('click', async () => {
     const tab = await getCurrentTab();
+    if (!tab.url.startsWith("https://bwd.xuanen.com.tw/wd02.aspx")) {
+        message.innerHTML = "You must at Da-Tong Sport center page.";
+        return;
+    }
+
     const startHours = reserveTime.value.split(',')
         .map(value => parseInt(value))
         .filter(num => (num > 4 && num < 22));
@@ -64,7 +74,7 @@ reserveButton.addEventListener('click', async () => {
         target: { tabId: tab.id },
         func: reserve,
         args: [reserveDate.value, startHours]
+    }).then(injectionResults => {
+        message.innerHTML = injectionResults[0].result
     });
-
-    message.innerHTML = "Done";
 });
